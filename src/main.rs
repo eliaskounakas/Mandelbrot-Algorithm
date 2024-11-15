c81575577ca105be3529f64aabc1bdda069eaba8
Elias Kounakas <eliaskounakas@proton.me>
2024-11-15 14:05:00 +0100

    remove dead code

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ mod client;
 
 fn main() {
     let input: (usize, usize, usize);
-    // uncomment and implement argument parsing, priting an error message in case of a parsing error
     match client::parse_args() {
         Ok(res) => {
             input = res;
@@ -25,16 +24,12 @@ fn main() {
 
     println!("Generating Mandelbrot for {}x{} image (max_iterations: {})", width, height, max_iterations);
 
-    // call mandelbrot::generate_image(width, height, max_iterations) and save the result to an image
     let mandelbrot_img = &generate_image(width, height, max_iterations);
    
-    // call the get_mandelbrot_pixels() method on the image struct and save the result in mandelbrot_pixel_count
     let mandelbrot_pixel_count = mandelbrot_img.get_mandelbrot_pixels();
 
-    // if the above line worked you should be able to uncomment this line
     println!("Pixels in the set: {}", mandelbrot_pixel_count);
     
-    // uncomment and call after you implement the mandelbrot functions, and handle the possible error
     save_to_file(mandelbrot_img, "mandelbrot.ppm");
 
 }
